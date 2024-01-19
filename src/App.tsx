fb8f7e4222feab23280d5eca575b0d040e9f11df
KimYuKyung <kyg0954@naver.com>
2024-01-19 15:41:51 +0900

    feat: login 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import GlobalStyle from "styles/GlobalStyle";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "@pages/Login";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <GlobalStyle />
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />}></Route>
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
