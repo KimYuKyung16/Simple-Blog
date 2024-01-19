3927414197bff8f3e0809df7016f3372cea62f81
KimYuKyung <kyg0954@gmail.com>
2024-01-19 15:56:00 +0900

    Revert "Feature/login"

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,7 @@
 import React from "react";
-import GlobalStyle from "styles/GlobalStyle";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "@pages/Login";
 
 function App() {
-  return (
-    <>
-      <GlobalStyle />
-      <Router>
-        <Routes>
-          <Route path="/login" element={<Login />}></Route>
-        </Routes>
-      </Router>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
--- src/styles/animation.ts
+++
@@ -1,9 +0,0 @@
-import { keyframes } from "styled-components";
-
-const wave_move = keyframes`
- 	100% {
-		transform: rotate(360deg);
-	}
-`;
-
-export { wave_move };
